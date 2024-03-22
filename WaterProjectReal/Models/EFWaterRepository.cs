f0a0f2df519f757afee85a3f772da35ae0f20de6
bristo23 <btstone23@gmail.com>
2024-03-22 17:32:35 -0600

    Add project files.

---
+++ WaterProjectReal/Models/EFWaterRepository.cs
@@ -0,0 +1,12 @@
+﻿
+namespace WaterProjectReal.Models
+{
+    public class EFWaterRepository : IWaterRepository
+    {
+        private WaterProjectContext _context;
+        public EFWaterRepository(WaterProjectContext temp) {
+            _context = temp;
+        }
+        public IQueryable<Book> Books => _context.Books;
+    }
+}
